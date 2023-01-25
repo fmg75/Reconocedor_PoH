77d2561af6c2a514d0cc8d0e4dc71ea1495bedf0
fmg75 <py.fmg.2022@gmail.com>
2023-01-25 13:37:32 -0300

    primera version

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from PIL import Image   
+from utils import *
+
+st.title('Reconocedor')
+
+def process_image(path):
+    face_net_models = FaceNetModels()
+    img = Image.open(path)
+    image_embedding = face_net_models.embedding(face_net_models.mtcnn(img))
+    distancia = face_net_models.Distancia(image_embedding)
+    return distancia[0][0],distancia[1][0]
+
+def upload_image():
+    uploaded_file = st.file_uploader("Elige una imagen", type=["jpg", "png"])
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        #st.image(image, caption='Imagen subida', use_column_width=True)
+        st.image(image, caption='Imagen subida', width=300)
+        label, distance = process_image(uploaded_file)
+
+        url = f"https://www.proofofhumanity.org/profile/{label}"
+        st.markdown(f'<a href="{url}" target="_blank">https://www.proofofhumanity.org/profile/{label}</a>', unsafe_allow_html=True)
+        st.write("Distancia Euclidiana: ", distance)
+
+upload_image()
