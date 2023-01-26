ff02fe3a663f1ac4b94e482ec8611c0f391d3524
fmg75 <py.fmg.2022@gmail.com>
2023-01-26 11:08:00 -0300

    texto arriba de la imagen cargada

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def process_image(path):
     return distancia[0][0],distancia[1][0]
 
 def upload_image():
-    uploaded_file = st.file_uploader("Elige una imagen que contenga solo un Rostro y verificara si tiene un perfil en https://www.proofofhumanity.org/profile/ ....", type=["jpg", "png"])
+    uploaded_file = st.file_uploader("Subir la imagen de un Humano y se verificara si esta registrado en https://www.proofofhumanity.org/", type=["jpg", "png"])
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
         #st.image(image, caption='Imagen subida', use_column_width=True)
