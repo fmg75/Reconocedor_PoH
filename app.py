dd3fee9a87d4e9455db692303ff452fc037b2ebd
fmg75 <py.fmg.2022@gmail.com>
2023-02-03 10:24:29 -0300

    se agrego un Try:

--- app.py
+++ app.py
@@ -13,22 +13,27 @@ from utils import *
 st.title('Esta Registrado en PoH ?')
 
 def process_image(path):
-    _models = FaceNetModels()
-    img = Image.open(path)
-    image_embedding = _models.embedding(_models.mtcnn(img))
-    distancia = _models.Distancia(image_embedding)
-    return distancia[0][0],distancia[1][0]
+    try:
+        _models = FaceNetModels()
+        img = Image.open(path)
+        image_embedding = _models.embedding(_models.mtcnn(img))
+        distancia = _models.Distancia(image_embedding)
+        return distancia[0][0],distancia[1][0]
+    except:
+        return None
 
 def upload_image():
     uploaded_file = st.file_uploader("Subir la imagen de un Humano y verificar si esta registrado en https://www.proofofhumanity.org/", type=["jpg", "png"])
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        #st.image(image, caption='Imagen subida', use_column_width=True)
         st.image(image, caption='Imagen subida', width=200)
-        label, distance = process_image(uploaded_file)
-
-        url = f"https://www.proofofhumanity.org/profile/{label}"
-        st.markdown(f'<a href="{url}" target="_blank">https://www.proofofhumanity.org/profile/{label}</a>', unsafe_allow_html=True)
-        st.write("Distancia Euclidiana: ", round(distance,4))
+        result = process_image(uploaded_file)
+        if result:
+            label, distance = result
+            url = f"https://www.proofofhumanity.org/profile/{label}"
+            st.markdown(f'<a href="{url}" target="_blank">https://www.proofofhumanity.org/profile/{label}</a>', unsafe_allow_html=True)
+            st.write("Distancia Euclidiana: ", round(distance,4))
+        else:
+            st.write("Algo falló con la imagen, intenta con otra !!")
 
 upload_image()
