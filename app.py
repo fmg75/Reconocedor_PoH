65db10ff0d281354ded3cc0ce65f9c0633eb00a1
fmg75 <py.fmg.2022@gmail.com>
2023-02-17 20:19:23 -0300

    favicon PoH

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import streamlit as st
 from PIL import Image   
 from utils import *
 
+# Configura el favicon de la aplicación
+favicon = open("logos/favicon.ico", "rb").read()
+st.set_page_config(page_title="Registrado en PoH?", page_icon=favicon)
+
 # agrego logo
 logo = Image.open('logos/democratic-poh-logo-text-hi-res-p-500.png')
 st.image(logo, use_column_width=True)
@@ -21,7 +25,7 @@ def process_image(path):
         return None
 
 def upload_image():
-    uploaded_file = st.file_uploader("Subir la imagen de un Humano y verificar si esta registrado en https://www.proofofhumanity.org/ o https://app.proofofhumanity.id/", type=["jpg", "png"])
+    uploaded_file = st.file_uploader("Subir la imagen de un Humano y verificar si esta registrado en https://www.proofofhumanity.org/", type=["jpg", "png"])
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
         st.image(image, caption='Imagen subida', width=200)
@@ -38,10 +42,11 @@ def upload_image():
     with st.expander('Información adicional'):
          st.write('Esta aplicación utiliza el modelo de redes neuronales conocido como ResNet '+
                  'para reconocer características de rostros en imágenes. Con esta tecnología se construyó un diccionario que la app utiliza ' + 
-                 'para comparar con las características del rostro ingresadas por el usuario. '+     
+                 'para comparar con las características del rostro ingresado por el usuario. '+     
                  'El usuario puede subir una imagen desde su dispositivo o utilizar la cámara para tomar una foto, y la aplicación '+ 
                  'devolverá el perfil de PoH más similar al de la base de datos junto con la distancia euclidiana entre los dos rostros.' +
                  ' Si la imagen corresponde a un humano registrado el rostro será reconocido en correspondencia con una distancia euclidiana muy baja, próxima a cero.'
-                 ' Por el momento se reconocen 16K de registrados en PoH, la base de datos se ira actualizando cada mil registrados nuevos.') 
+                 ' Por el momento se reconocen 16 mil de registrados en PoH, la base de datos se ira actualizando cada mil registrados nuevos.') 
 
+#lanza app
 upload_image()
