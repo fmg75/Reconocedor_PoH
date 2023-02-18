4482dca42f14f27d36092b8ac39dd41bb14838a7
fmg75 <py.fmg.2022@gmail.com>
2023-02-18 19:15:20 -0300

    nada

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def upload_image():
     uploaded_file = st.file_uploader("Subir la imagen de un Humano y verificar si esta registrado en https://www.proofofhumanity.org/", type=["jpg", "png"])
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        st.image(image, caption='Imagen subida', width=200)
+        st.image(image, caption='Imagen subida ', width=200)
         result = process_image(uploaded_file)
         if result:
             label, distance = result
