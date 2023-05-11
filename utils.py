b0d59496815ad57f5832c279b4eb4c7ad7d1f545
fmg75 <py.fmg.2022@gmail.com>
2023-05-11 16:40:17 -0300

    simplifica

--- utils.py
+++ utils.py
@@ -14,15 +14,22 @@ class FaceNetModels:
         img_embedding = self.model(img_tensor.unsqueeze(0))
         return img_embedding
 
-    def Distancia (self,img_embedding):
-        distances = []
-        for label, emb in self.caracteristicas.items():
-            distance = torch.dist(emb, img_embedding)
-            distances.append((label, distance))
-        # ordena la lista distancia de menor a mayor
-        sorted_distances = sorted(distances, key=lambda x: x[1])
-        # obtener los untimos valores de E y D
-        E = [sorted_distances[i][0] for i in range(0,1,1)]
-        D = [sorted_distances[i][1].item() for i in range(0,1,1)]
+    # def Distancia (self,img_embedding):
+    #     distances = []
+    #     for label, emb in self.caracteristicas.items():
+    #         distance = torch.dist(emb, img_embedding)
+    #         distances.append((label, distance))
+    #     # ordena la lista distancia de menor a mayor
+    #     sorted_distances = sorted(distances, key=lambda x: x[1])
+    #     # obtener los untimos valores de E y D
+    #     E = [sorted_distances[i][0] for i in range(0,1,1)]
+    #     D = [sorted_distances[i][1].item() for i in range(0,1,1)]
 
-        return E, D
+    #     return E, D
+
+    def Distancia(self, img_embedding):
+        distances = [(label, torch.dist(emb, img_embedding)) for label, emb in self.caracteristicas.items()]
+        sorted_distances = sorted(distances, key=lambda x: x[1])
+        # E = sorted_distances[0][0]
+        # D = sorted_distances[0][1].item()
+        return sorted_distances[0][0], sorted_distances[0][1].item()
