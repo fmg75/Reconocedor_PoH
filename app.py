bf1645c860b83f1aeab74b413e6adf6707e9499a
fmg75 <f.mg@outlook.com>
2024-02-01 12:49:35 -0300

    14.28

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def upload_image():
                 f'<a href="{url}" target="_blank">https://app.proofofhumanity.id/profile/{label}</a>',
                 unsafe_allow_html=True,
             )
-            st.write("% Similitud: ", int(100- 14,28*distance))
+            st.write("% Similitud: ", int(100- 14.28*distance))
         else:
             st.write(
                 "Something went wrong with the provided image, please try another!!"
